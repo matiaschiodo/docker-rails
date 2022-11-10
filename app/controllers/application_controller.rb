45fdd86a0954d393510c481c4936b5ad4092b675
Nahuel <nahuelignacio.dominguez@gmail.com>
2022-11-10 15:03:03 -0300

    Se crearon los archivos de la internacionalizacion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
 
     before_action :authenticate_user!
+    before_action :set_locale
+
+    def set_locale
+        I18n.locale = 'es'
+    end
 end
