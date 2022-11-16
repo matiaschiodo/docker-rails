e63e3214034f075a94b698493c076994c9a31303
Nahuel <nahuelignacio.dominguez@gmail.com>
2022-11-16 15:18:33 -0300

    Continuando con las traducciones

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :budgets
   devise_for :users
   get 'home' => 'home#index'
+  root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
