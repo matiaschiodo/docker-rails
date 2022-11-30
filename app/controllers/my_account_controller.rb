e4db6f692587b34a31e4db04a3d58d30b485175b
Nahuel <nahuelignacio.dominguez@gmail.com>
2022-11-30 15:35:18 -0300

    principio del fin

---
+++ app/controllers/my_account_controller.rb
@@ -0,0 +1,4 @@
+class MyAccountController < ApplicationController
+    def index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home' => 'home#index'
   root to: 'home#index'
+  get 'my_account' => 'my_account#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
